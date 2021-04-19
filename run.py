9c535cf633167da6b53ea7d02024000c8bbdf49d
hiyoung123 <hiyoungliu@gmail.com>
2021-04-19 14:40:05 +0800

    add

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 #-*- coding:utf-8 -*-
 
 import argparse
+import os
+
 from src.train import run_train
 from src.evaluation import run_eval
 from src.inference import run_inference
@@ -12,6 +14,17 @@ def main(args):
     config = Config.parse(args.model)
     for k, v in vars(args).items():
         config.update({k: v})
+
+    model_path = os.path.join(config.model_path, config.model + '_' + config.task_name)
+    if not os.path.exists(model_path):
+        os.mkdir(model_path)
+
+    result_path = os.path.join(config.result_path, config.model + '_' + config.task_name)
+    if not os.path.exists(model_path):
+        os.mkdir(result_path)
+
+    config.model_path = model_path + '/best_model.pt'
+    config.result_path = result_path + '/result.csv'
 
     if args.do_train:
         run_train(config)

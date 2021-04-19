6abf52ca9c4fed0f63e09b70ce2c547abf4bec37
hiyoung123 <hiyoungliu@gmail.com>
2021-04-19 15:35:13 +0800

    add

---
+++ run.sh
@@ -0,0 +1,49 @@
+python run.py --model TextCNN --save_by_step True --patience 1000
+python run.py --model TextRNN --save_by_step True --patience 1000
+python run.py --model TextRCNN --save_by_step True --patience 1000
+python run.py --model DPCNN --save_by_step True --patience 1000
+
+python run.py --model BertFC --save_by_step True --patience 1000
+python run.py --model BertCNN --save_by_step True --patience 1000
+python run.py --model BertRNN --save_by_step True --patience 1000
+python run.py --model BertRCNN --save_by_step True --patience 1000
+
+python run.py --model TextCNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+python run.py --model TextRNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+python run.py --model TextRCNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+python run.py --model DPCNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+
+python run.py --model BertFC --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+python run.py --model BertCNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+python run.py --model BertRNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+python run.py --model BertRCNN --save_by_step True --patience 1000 --init_weight True --task_name init_weight
+
+python run.py --model TextCNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+python run.py --model TextRNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+python run.py --model TextRCNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+python run.py --model DPCNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+
+python run.py --model BertFC --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+python run.py --model BertCNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+python run.py --model BertRNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+python run.py --model BertRCNN --save_by_step True --patience 1000 --loss_type focal_loss --task_name focal_loss
+
+python run.py --model TextCNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+python run.py --model TextRNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+python run.py --model TextRCNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+python run.py --model DPCNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+
+python run.py --model BertFC --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+python run.py --model BertCNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+python run.py --model BertRNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+python run.py --model BertRCNN --save_by_step True --patience 1000 --loss_type label_smooth --task_name label_smooth
+
+python run.py --model TextCNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+python run.py --model TextRNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+python run.py --model TextRCNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+python run.py --model DPCNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+
+python run.py --model BertFC --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+python run.py --model BertCNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+python run.py --model BertRNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
+python run.py --model BertRCNN --save_by_step True --patience 1000 --lookahead True --task_name lookahead
